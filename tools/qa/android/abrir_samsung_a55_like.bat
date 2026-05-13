75199220a4617d200f2330629ae5627ae3594ad3
Lucas Mateus Soares de Lima <lucasmslima1@gmail.com>
2026-05-13 03:23:33 -0300

    Criação de scripts de QA Android para Samsung A55-like

---
+++ tools/qa/android/abrir_samsung_a55_like.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==================================================
+echo Abrindo Emulador: Samsung_A55_Like (QA ConeCTEA)
+echo Modo: ANGLE (GPU Indirect)
+echo ==================================================
+start "" "%LOCALAPPDATA%\Android\Sdk\emulator\emulator.exe" -avd Samsung_A55_Like -gpu angle_indirect
+echo Emulador enviado para o background.
+pause
---
+++ tools/qa/android/rodar_conectea_samsung_a55_like.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==================================================
+echo Rodando ConeCTEA no Samsung_A55_Like
+echo ATENCAO: O emulador deve estar aberto (emulator-5554)
+echo ==================================================
+cd /d H:\Sites\ConeCTEA
+flutter run -d emulator-5554
+pause
