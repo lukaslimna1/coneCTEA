19f8e6471df3005b79326cb058e7894341150b14
Lucas Mateus Soares de Lima <lucasmslima1@gmail.com>
2026-05-13 21:46:26 -0300

    Criação de scripts de QA Android para perfis Small e Medium

---
+++ tools/qa/android/abrir_android_medium_412dp.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==================================================
+echo Abrindo Emulador: Android_Medium_412dp_QA
+echo Modo: ANGLE (GPU Indirect)
+echo ==================================================
+start "" "%LOCALAPPDATA%\Android\Sdk\emulator\emulator.exe" -avd Android_Medium_412dp_QA -gpu angle_indirect
+echo Emulador enviado para o background.
+pause
---
+++ tools/qa/android/rodar_conectea_android_small_360dp.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==================================================
+echo Rodando ConeCTEA no Android_Small_360dp_QA
+echo ATENCAO: O emulador deve estar aberto (emulator-5554)
+echo ==================================================
+cd /d H:\Sites\ConeCTEA
+flutter run -d emulator-5554
+pause
