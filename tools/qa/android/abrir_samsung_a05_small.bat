88d79811a0f46747173f0bc0a94a625bc3fc300c
Lucas Mateus Soares de Lima <lucasmslima1@gmail.com>
2026-05-13 21:01:33 -0300

    Criação de scripts de QA Android para Samsung A05-small

---
+++ tools/qa/android/abrir_samsung_a05_small.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==================================================
+echo Abrindo Emulador: Samsung_A05_Small (QA ConeCTEA)
+echo Modo: ANGLE (GPU Indirect)
+echo ==================================================
+start "" "%LOCALAPPDATA%\Android\Sdk\emulator\emulator.exe" -avd Samsung_A05_Small -gpu angle_indirect
+echo Emulador enviado para o background.
+pause
